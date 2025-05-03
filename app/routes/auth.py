7a57a9062aa52f3449d1726dde199599c6374829
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-05-03 09:08:17 +0530

    chore(auth): remove unused make_response import

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
